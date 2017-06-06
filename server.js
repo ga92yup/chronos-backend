e973c00e91101b3e13b704fe7390011c6c9e9b2b
Franz Volland <fra.volland@tum.de>
2017-06-06 10:03:11 +0200

    Add initial backend

---
+++ server.js
@@ -0,0 +1,16 @@
+var Config = require('./config/config');
+var app = require('./app');
+
+/**
+ * Start the server
+  */
+app.listen(Config.app.port);
+console.log("Server is listening on port %s", Config.app.port);
+
+
+
+
+
+
+
+
