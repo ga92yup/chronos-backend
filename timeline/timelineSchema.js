795a488ad4e0b3368e6131eeba972f9888e25779
Milena <ga92yup@mytum.de>
2017-06-12 01:18:55 +0200

    service for timeline creation - version 1

---
+++ timeline/timelineSchema.js
@@ -0,0 +1,16 @@
+// Load required packages
+var mongoose = require('mongoose');
+
+// Define our movie schema
+var Timeline   = new mongoose.Schema({
+    title: String,
+    year: Number,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+// Export the Mongoose model
+module.exports = mongoose.model('Timeline', Timeline);
+
