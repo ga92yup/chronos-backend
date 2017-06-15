9b3930c145f7a4a6fee24bb485353892ce74107d
fravoll <franz.volland@freenet.de>
2017-06-15 12:50:56 +0200

    Revert "Edit user schema to include email and premium status"

--- user/userSchema.js
+++ user/userSchema.js
@@ -10,14 +10,6 @@ var userSchema = mongoose.Schema({
     password: {
         type: String,
         required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    premiumStatus: {
-        type: Boolean,
-        required: true
     }
 });
 
